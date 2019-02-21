d3a9649bdc8c771bd112712f4b87d2546a04a5e3
moritzfelipe <stellmacher@gmail.com>
2019-02-21 16:28:01 +0100

    simplefied order function

--- index.js
+++ index.js
@@ -20,16 +20,12 @@
     }
 
     //Function that orders memes so that the meme with the most votes is on top
-    function orderMemes(memeArrayUnordered) {
-      function compare(a,b) {
-        if (a.votes > b.votes)
-          return -1;
-        if (a.votes < b.votes)
-          return 1;
-        return 0;
-      }
-      let memeArrayOrdered = memeArrayUnordered.sort(compare);
-      return memeArrayOrdered
+    function compare(a,b) {
+      if (a.votes > b.votes)
+        return -1;
+      if (a.votes < b.votes)
+        return 1;
+      return 0;
     }
 
     //Function to execute the vote call when a user votes with ae on a meme
@@ -101,7 +97,7 @@
       $("#loader").hide();
 
       //Order the memes array so that the meme with the most objects is on top
-      memeArray = orderMemes(memeArray);
+      memeArray = memeArray.sort(compare);
       //Display all memes in an html list
       displayMemeList(memeArray);
     }
