194ce108a4f2cbb5300549172092679e1542648f
moritzfelipe <stellmacher@gmail.com>
2019-02-26 10:45:32 +0100

    different stuff

--- index.js
+++ index.js
@@ -19,6 +19,13 @@
       });
     }
 
+    function renderMemes() {
+      var template = $('#template').html();
+      Mustache.parse(template);   // optional, speeds up future uses
+      var rendered = Mustache.render(template, memeArray);
+      $('#memeBody').html(rendered);
+    }
+
     //Function that orders memes so that the meme with the most votes is on top
     function compare(a,b) {
       if (a.votes > b.votes)
@@ -27,35 +34,6 @@
         return 1;
       return 0;
     }
-
-    //Function to execute the vote call when a user votes with ae on a meme
-    async function voteCall(index,inputAmount) {
-      //Display the loader animation so the user knows that something is happening
-      $("#loader").show();
-      //Make the async call to the blockchain with index of the meme and amount in attos
-      const calledSet = await client.contractCall(contractAddress, 'sophia-address', contractAddress, 'voteMeme', {args: '('+index+')',options: {amount: inputAmount}}).catch(async e => {
-      console.error(e);
-      //If there is an error decode and console log it
-      const decodedError = await client.contractDecodeData('string', e.returnValue).catch(e => console.error(e));
-      console.log('decodedError', decodedError);
-      });
-      //Hide the loading animation after async calls return a value
-      $("#loader").hide();
-      //Reload the page so the new voting value can be shown
-      location.reload();
-    };
-
-    //Function to register a meme after user submitted form, similar to vote function
-    async function registerCall(url,name) {
-      $("#loader").show();
-      const calledSet = await client.contractCall(contractAddress, 'sophia-address', contractAddress, 'registerMeme', {args: '("'+url+'","'+name+'")'}).catch(async e => {
-        console.error(e);
-        const decodedError = await client.contractDecodeData('string', e.returnValue).catch(e => console.error(e));
-        console.log('decodedError', decodedError);
-      });
-      $("#loader").hide();
-      location.reload();
-    };
 
     //Execute main function
     window.addEventListener('load', async () => {
@@ -69,7 +47,7 @@
       const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', 'getMemesLength ', {args: '()'}).catch(e => console.error(e));
       console.log('calledGet', calledGet);
 
-      const decodedGet = await client.contractDecodeData('int', calledGet.result).catch(e => console.error(e));
+      const decodedGet = await client.contractDecodeData('int', calledGet.result.returnValue).catch).catch(e => console.error(e));
       console.log('decodedGet1', decodedGet.value);
 
       //Pass the int value of meme length to a const
@@ -82,7 +60,7 @@
       for (let i = 1; i < length+1; i++) {
         //Make the call to the blockchain to get all relevant information on the meme
         const calledGet = await client.contractCallStatic(contractAddress, 'sophia-address', 'getMeme ', {args: '('+i+')'}).catch(e => console.error(e));
-        const decodedGet = await client.contractDecodeData('(address, string, string, int)', calledGet.result).catch(e => console.error(e));
+        const decodedGet = await client.contractDecodeData('(address, string, string, int)', calledGet.result.returnValue).catch(e => console.error(e));
 
         //Create a new element with all the relevant information for the meme and push the new element into the array with all memes
         memeArray.push({
@@ -98,22 +76,43 @@
       //Order the memes array so that the meme with the most objects is on top
       memeArray = memeArray.sort(compare);
       //Display all memes in an html list
-      displayMemeList(memeArray);
+      renderMemes();
     });
 
     //If someone clicks to vote on a meme, get the input and execute the voteCall
-    jQuery("#memeBody").on("click", ".voteBtn", function(){
-      var input = $(this).closest('div'),
-          index = input.attr('index'),
-          inputValue = input.find('input[name="input1"]').val();
+    jQuery("#memeBody").on("click", ".voteBtn", async function(event){
+      let value = $(this).siblings('input').val();
+      let id = event.target.id;
+          inputAmount = input.find('input[name="input1"]').val();
 
-      voteCall(index,inputValue);
+      //Display the loader animation so the user knows that something is happening
+      $("#loader").show();
+      //Make the async call to the blockchain with index of the meme and amount in attos
+      const calledSet = await client.contractCall(contractAddress, 'sophia-address',
+            contractAddress, 'voteMeme', {args: '('+index+')',options: {amount: inputAmount}}).catch(async e => {
+      console.error(e);
+      //If there is an error decode and console log it
+      const decodedError = await client.contractDecodeData('string', e.returnValue).catch(e => console.error(e));
+      console.log('decodedError', decodedError);
+      });
+      //Hide the loading animation after async calls return a value
+      $("#loader").hide();
+      //update and render memes
+      renderMemes();
     });
 
     //If someone clicks to register a meme, get the input and execute the registerCall
-    $('#registerBtn').click(function() {
+    $('#registerBtn').click(async function(){
       var name = ($('#regName').val()),
           url = ($('#regUrl').val());
 
-      registerCall(url,name);
+      $("#loader").show();
+      const calledSet = await client.contractCall(contractAddress, 'sophia-address', contractAddress, 'registerMeme', {args: '("'+url+'","'+name+'")'}).catch(async e => {
+        console.error(e);
+        const decodedError = await client.contractDecodeData('string', e.returnValue).catch(e => console.error(e));
+        console.log('decodedError', decodedError);
+      });
+      $("#loader").hide();
+      //update and render meme
+      renderMemes();
     });
