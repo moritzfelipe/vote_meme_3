46d4a2f09b229531ebbebdd08733169588ca86fa
moritzfelipe <stellmacher@gmail.com>
2019-02-27 09:32:49 +0100

    test

--- index.js
+++ index.js
@@ -101,10 +101,18 @@
 
       const calledSet = await client.contractCall(contractAddress, 'sophia-address',
             contractAddress, 'voteMeme', {args,
-            options: {amount: value}}).catch(async e => {
+            options}).catch(async e => {
       const decodedError = await client.contractDecodeData('string',
             e.returnValue).catch(e => console.error(e));
       });
+
+
+      // const calledSet = await client.contractCall(contractAddress, 'sophia-address',
+      //       contractAddress, 'voteMeme', {args,
+      //       options: {amount: value}}).catch(async e => {
+      // const decodedError = await client.contractDecodeData('string',
+      //       e.returnValue).catch(e => console.error(e));
+      // });
 
       console.log(calledSet);
       //Hide the loading animation after async calls return a value
